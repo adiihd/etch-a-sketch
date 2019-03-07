1e2b82214abe5b020cd3d0090aed5eb5cf115eed
Adrian Chirculescu <adrianchirculescu@gmail.com>
2019-03-07 14:44:34 +0000

    html - change buttons div - change buttons js - cahce dom

---
+++ app.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#container");
+let start = document.querySelector("#start");
+const black = document.querySelector("#black");
+const color = document.querySelector("#start");
+const div = document.createElement('div');
+
+container.appendChild(div);
