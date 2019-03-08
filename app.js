360d046ea4efba603a9b893cecb4745e06a888d6
Adrian Chirculescu <adrianchirculescu@gmail.com>
2019-03-08 14:40:46 +0000

    finish

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-const container = document.querySelector("#container");
-let start = document.querySelector("#start");
-const black = document.querySelector("#black");
-const color = document.querySelector("#start");
-const div = document.createElement('div');
+const createClearBtn = document.querySelector('#start')
+const colorBtn = document.querySelector('#color')
+const gridContainer = document.querySelector('.grid-container')
+var pixelList
+
+createClearBtn.addEventListener('click', createGrid)
+colorBtn.addEventListener('click', randomColorListener)
 
 
+function addListener (e) {
+  e.addEventListener('mouseover', function (event) {
+    event.target.classList.add('permhover')
+  })
+}
 
-function startGrid(size){
-    for(let i = 0; i < size; i++){
-        container.appendChild(div);
+function createGrid () {
+  if (!gridContainer.hasChildNodes()) {
+    let i
+    let gridPixel
+    let gridSize = prompt('Enter the size of grid you want!')
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+
+    for (i = 0; i < (gridSize ** 2); i++) {
+      gridPixel = document.createElement('div')
+      gridPixel.style.height = (600 / gridSize).toString() + 'px'
+      gridPixel.classList.add('pixel')
+      gridContainer.appendChild(gridPixel)
     }
+    pixelList = document.querySelectorAll('.pixel')
+    pixelList.forEach(addListener)
+  } else {
+    while (gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.firstChild)
+    }
+  }
+}
+
+function getRandomColor () {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+
+function randomColorListener () {
+  function addColorListener (e) {
+    e.addEventListener('mouseover', function (event) {
+      event.target.style.backgroundColor = getRandomColor()
+    })
+  }
+  pixelList.forEach(addColorListener)
 }
